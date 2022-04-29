a3b58edfc7852ebc2bcb19ef9f91f1d3dd7134fc
mznmk <mika_develop@mznmk.com>
2022-04-30 03:14:14 +0900

    [c_compiler] finish "step1"
    ステップ１：整数1個をコンパイルする言語の作成 完了

---
+++ C_Compiler/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+assert()
+{
+    expected="$1"
+    input="$2"
+
+    # [ execute script ]
+    ./9cc "$input" > tmp.s
+    cc tmp.s -o tmp
+    ./tmp
+    actual="$?"
+
+    # [ compare ]
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK
