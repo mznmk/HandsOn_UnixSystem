ee76970b51569cb99fe45d364cb0c5e806d0a410
mznmk <mika_develop@mznmk.com>
2022-05-01 20:26:35 +0900

    [c_compiler] finish "step5"
    ステップ5：四則演算のできる言語の作成 完了

---
+++ C_Compiler/step05/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# [ assert function ]
+assert()
+{
+    expected="$1"
+    input="$2"
+
+    # [ execute script ]
+    ./9cc "$input" > tmp.s
+    cc tmp.s -o tmp
+    ./tmp
+    actual="$?"
+
+    # [ compare ]
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+# [ test ]
+# step1
+assert 0 0
+assert 42 42
+# step2
+assert 21 "5+20-4"
+# step3
+assert 41 " 12 + 34 - 5 "
+# step5
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
+
+# [ success ]
+echo OK
